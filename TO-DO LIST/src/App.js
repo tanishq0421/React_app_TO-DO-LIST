3eaee6465853acb0d3d6df3eb74965e6dd6ecd6a
Tanishq Shukla <tanishq.shukla.phe21@iitbhu.ac.in>
2022-10-14 11:38:01 +0530

    To-do react app created

---
+++ TO-DO LIST/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './MyComponents/Header';
+
+function App() {
+  return (
+    <>
+      <div>
+      <Header/>
+      <h3> My App </h3>
+      <p>My App Works</p> 
+      </div>
+      
+    </>
+  );
+}
+
+export default App;
